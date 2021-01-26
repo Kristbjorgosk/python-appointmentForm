6ad582c80b6f8c7f7000a7d1073452c6fa524308
kristbjörg <kristbjorg_@hotmail.com>
2021-01-26 03:40:34 +0000

    commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newbooking.db'
+db = SQLAlchemy(app)
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer = db.Column(db.String(200), unique=True)
+    staff = db.Column(db.String(200))
+    comments = db.Column(db.Text())
+
+    def __init__(self, customer, staff, comments):
+        self.customer = customer
+        self.staff = staff
+        self.comments = comments
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/submit', methods=["GET", 'POST'])
+def submit():
+
+    if request.method == 'POST':
+        customer = request.form['customer']
+        staff = request.form['staff']
+        comments = request.form['comments']
+        #print(customer, staff, comments)
+        return render_template("sent.html")
+        if customer == '' or staff == '':
+            return render_template('index.html', message='Please enter required fields')
+        if db.session.query(Booking).filter(Booking.customer == customer).count() == 0:
+            data = Booking(customer, staff, comments)
+            db.session.add(data)
+            db.session.commit()
+            # send_mail(customer, staff, comments)
+            return render_template('sent.html')
+        return render_template('index.html', message='You have already booked an appointment')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Python & Flask
+
+I did a small hair salon appointment form to submit. After it is submitted the data will appear in database with sqlite and DB browser for sqlite
+
+For styling I used Bootstrap and deployed the app with Heroku
+
+[Live version is here]()
+# python-appointmentForm
